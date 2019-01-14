494dbe96a3650913cfdab4292a6b46df4d433819
OnnoEbbens <onnoebbens@gmail.com>
2019-01-14 19:06:45 +0100

    update whatsapp exercise

--- Practical_examples/whatsapp data/whatsapp_func.py
+++ Practical_examples/whatsapp data/whatsapp_func.py
@@ -1,4 +1,3 @@
-# Cleaning WhatsApp log
 import os
 import re
 import pandas as pd
@@ -6,7 +5,24 @@ import numpy as np
 import zipfile
 
 
-def read_whatsapp(whatsapp_file, pattern=None, ):
+
+def strftime_to_re_pattern(strftime_format):
+    re_pattern = strftime_format.replace('%d', '\\d\\d')
+    re_pattern = re_pattern.replace('%m', '\\d\\d')
+    re_pattern = re_pattern.replace('%Y', '\\d\\d\\d\\d')
+    re_pattern = re_pattern.replace('%H', '\\d\\d')
+    re_pattern = re_pattern.replace('%M', '\\d\\d')
+    re_pattern = re_pattern.replace('%S', '\\d\\d')
+    re_pattern = re_pattern.replace('/', '\\/')
+    re_pattern = re_pattern.replace('[', '\\[')
+    re_pattern = re_pattern.replace(']', '\\]')
+    re_pattern = '(' + re_pattern + ')'
+
+    return re_pattern
+
+def read_whatsapp(whatsapp_file, datetime_pattern = '(\[\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d\])',
+                  user_sep=':', strftime_format = '[%d/%m/%Y, %H:%M:%S]',
+                  encoding="utf8"):
     """
     read a whatsapp data file into a pandas dataframe
 
@@ -15,63 +31,69 @@ def read_whatsapp(whatsapp_file, pattern=None, ):
     whatsapp_file: str
         path of the exported data from Whatsapp, can be a .zip or .txt file
 
-    pattern: _sre.SRE_Pattern
-        a compiled regular expression used to find the date format
-
     Returns
     -------
     time_user_df: pandas.DataFrame
         DataFrame with all the data from you whatsapp conversation
     """
 
-    if whatsapp_file.endswith('.zip'):
+    #check file type
+    if whatsapp_file.endswith('txt'):
+        whatsapp_txt = whatsapp_file
+    elif whatsapp_file.endswith('.zip'):
         whatsapp_txt = unzip_whatsapp_file(whatsapp_file)
     else:
-        whatsapp_txt = whatsapp_file
+        raise FileNotFoundError('could not open file: %s'%whatsapp_file)
 
-    # define regulare expressions
-    pattern_en = re.compile('(\[\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d\])\s+(.+)')
-    pattern_nl = re.compile('(\[\d\d\-\d\d\-\d\d \d\d:\d\d:\d\d\])\s+(.+)')
-    group_pattern_en = 'Messages to this group are now secured with end-to-end encryption'
-    group_pattern_nl = 'Berichten en oproepen in deze chat zijn nu beveiligd met end-to-end encryptie'
+    re_datetime = re.compile(datetime_pattern)
+    datetimeuser_pattern = datetime_pattern + '(.*?)' + user_sep
+    re_datetimeuser = re.compile(datetimeuser_pattern)
 
-    # find usernames
-    user_names = []
-    datetime_list = []
-    user_list = []
-    text_list = []
-    with open(whatsapp_txt, 'r', encoding="utf8") as fo:
+    with open(whatsapp_txt, 'r', encoding=encoding) as fo:
         line = fo.readline()
-        if group_pattern_en in line:
-            pattern = re.compile('(\[\d\d\/\d\d\/\d\d\d\d, \d\d:\d\d:\d\d\])\s+(.+)')
-            print('recognised English file format')
-            for i in range(2):
-                fo.readline()
+        check_date_pattern(line, re_datetime)
+        check_date_user_pattern(line, re_datetimeuser)
 
-        elif group_pattern_nl in line:
-            pattern = re.compile('(\[\d\d\-\d\d\-\d\d \d\d:\d\d:\d\d\])\s+(.+)')
-            print('recognised Dutch file format')
-            for i in range(2):
-                fo.readline()
+        re_datetimeuser.split(line)
+
+        empty, date, user, message = re_datetimeuser.split(line)
+        datetime_list = [date]
+        user_list = [user]
+        message_list = [message]
 
         for line in fo:
-            pattern_match = pattern.search(line)
-            if pattern_match and ':' in pattern_match.group(2):
-                user_name = pattern_match.group(2).split(':')[0]
-                if user_name not in user_names:
-                    user_names.append(user_name)
-                user_list.append(user_name)
-                datetime_list.append(pattern_match.group(1)[1:-1])
-                text_list.append(":".join(pattern_match.group(2).split(':')[1:]))
+            pattern_match = re_datetimeuser.search(line)
+            if pattern_match:
+                empty, date, user, message = re_datetimeuser.split(line)
+                datetime_list.append(date)
+                user_list.append(user)
+                message_list.append(message)
             else:
-                text_list[-1] = text_list[-1] + line
+                message_list[-1] = message_list[-1] + line
 
-    time_user_df = pd.DataFrame(data={'user': user_list,
-                                      'text': text_list,
-                                      'message': [1] * len(user_list)},
-                                index=pd.to_datetime(datetime_list, dayfirst=True))
+    time_user_df = pd.DataFrame(data={'datetime': datetime_list,
+                                      'user': user_list,
+                                      'text': message_list,
+                                      'message': [1] * len(user_list)}, )
+
+    time_user_df.index = pd.to_datetime(time_user_df.datetime,
+                                        format=strftime_format, dayfirst=True)
 
     return time_user_df
+
+
+def check_date_pattern(line, re_datetime):
+    if re_datetime.search(line):
+        print('date pattern recognized')
+    else:
+        raise ValueError('pattern not recgonized')
+
+
+def check_date_user_pattern(line, re_datetimeuser):
+    if re_datetimeuser.search(line):
+        print('user separator recognized')
+    else:
+        raise ValueError('pattern not recgonized')
 
 
 def check_anonymized_dataset(time_user_df):
@@ -110,9 +132,10 @@ def unzip_whatsapp_file(whatsapp_file):
     """
 
     zip_ref = zipfile.ZipFile(whatsapp_file, 'r')
+    txt_file = zip_ref.namelist()[0]
     zip_ref.extractall(os.path.split(whatsapp_file)[0])
     zip_ref.close()
 
     zip_dir = os.path.split(whatsapp_file)[0]
 
-    return os.path.join(zip_dir, '_chat.txt')
+    return os.path.join(zip_dir, txt_file)
