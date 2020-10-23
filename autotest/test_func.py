bce46c017b21eda69806c0e56ea1b47217be4272
onno ebbens <onnoebbens@gmail.com>
2020-10-23 10:50:48 +0200

    remove metadata from cells during tests

--- autotest/test_func.py
+++ autotest/test_func.py
@@ -96,7 +96,7 @@ def run_notebook(fname, clearoutput=True, timeout=120):
     
     print('running --> {}'.format(fname))
     
-    cmd_args = f'jupyter nbconvert --ExecutePreprocessor.timeout={timeout} --to notebook --execute "{fname}" --output "{fname}"'
+    cmd_args = f'jupyter nbconvert --ExecutePreprocessor.timeout={timeout} --to notebook --execute "{fname}" --output "{fname}" --ClearMetadataPreprocessor.enabled=True'
     
     out = os.system(cmd_args)
     
