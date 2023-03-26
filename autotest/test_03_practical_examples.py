e5b488b8d1416389053c4a8b00f4a40814e97138
Petra Izeboud <petra.izeboud@mamba-python.nl>
2023-03-26 21:20:52 +0200

    Let test_03_practical_examples comply tot PEP8

--- autotest/test_03_practical_examples.py
+++ autotest/test_03_practical_examples.py
@@ -1,10 +1,10 @@
 import os
-import sys
 import test_func as tf
 
-#make projectdir accessible inside this script
+# make projectdir accessible inside this script
 tst_dir = os.path.dirname(os.path.realpath(__file__))
-nb_dir = os.path.join(tst_dir, "..", *('', 'practical_examples'))
+nb_dir = os.path.join(
+    tst_dir, "..", *('', 'practical_examples'))
 
 # def test_AHN_downloader():
 #     fpath_rel = ['01_GIS', '01_AHN_downloader', 'AHN downloader.ipynb']
@@ -13,7 +13,8 @@ nb_dir = os.path.join(tst_dir, "..", *('', 'practical_examples'))
 #     return 0
 
 # def test_Delfland_waterways():
-#     fpath_rel = ['01_GIS', '02_Delfland_waterways', 'Delfland_waterways.ipynb']
+#     fpath_rel = ['01_GIS', '02_Delfland_waterways',
+#                       'Delfland_waterways.ipynb']
 #     fname = os.path.join(nb_dir, *fpath_rel)
 #     tf.run_notebook(fname, clearoutput=False)
 #     return 0
@@ -24,23 +25,31 @@ nb_dir = os.path.join(tst_dir, "..", *('', 'practical_examples'))
 #     tf.run_notebook(fname, clearoutput=False)
 #     return 0
 
+
 def test_geboortes_nl():
     fpath_rel = ['02_data_analysis', '01_cbs_data', 'geboortes_nl.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_CO2_emissions_map():
-    fpath_rel = ['02_data_analysis', '02_CO2_emissions', 'CO2_emissions_map.ipynb']
+    fpath_rel = [
+        '02_data_analysis', '02_CO2_emissions', 'CO2_emissions_map.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_congress_twitter_analysis():
-    fpath_rel = ['02_data_analysis', '03_congress_twitter_analysis', 'congress_twitter_analysis.ipynb']
+    fpath_rel = [
+        '02_data_analysis', '03_congress_twitter_analysis',
+        'congress_twitter_analysis.ipynb'
+        ]
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_00___Introduction_and_Setup():
     fpath_rel = ['03_bokeh', '00 - Introduction and Setup.ipynb']
@@ -48,11 +57,13 @@ def test_00___Introduction_and_Setup():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_01___Basic_Plotting():
     fpath_rel = ['03_bokeh', '01 - Basic Plotting.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_02___Styling_and_Theming():
     fpath_rel = ['03_bokeh', '02 - Styling and Theming.ipynb']
@@ -60,11 +71,13 @@ def test_02___Styling_and_Theming():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_03___Data_Sources_and_Transformations():
     fpath_rel = ['03_bokeh', '03 - Data Sources and Transformations.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_04___Adding_Annotations():
     fpath_rel = ['03_bokeh', '04 - Adding Annotations.ipynb']
@@ -72,11 +85,13 @@ def test_04___Adding_Annotations():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_05___Presentation_Layouts():
     fpath_rel = ['03_bokeh', '05 - Presentation Layouts.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_06___Linking_and_Interactions():
     fpath_rel = ['03_bokeh', '06 - Linking and Interactions.ipynb']
@@ -84,11 +99,13 @@ def test_06___Linking_and_Interactions():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_07___Bar_and_Categorical_Data_Plots():
     fpath_rel = ['03_bokeh', '07 - Bar and Categorical Data Plots.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_08___Graph_and_Network_Plots():
     fpath_rel = ['03_bokeh', '08 - Graph and Network Plots.ipynb']
@@ -96,11 +113,13 @@ def test_08___Graph_and_Network_Plots():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_11___Running_Bokeh_Applications():
     fpath_rel = ['03_bokeh', '11 - Running Bokeh Applications.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_Folium02_CheckZorder():
     fpath_rel = ['04_folium', 'Folium02_CheckZorder.ipynb']
@@ -108,11 +127,13 @@ def test_Folium02_CheckZorder():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_Folium03_Colormaps():
     fpath_rel = ['04_folium', 'Folium03_Colormaps.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_Folium04_ContinuousWorld():
     fpath_rel = ['04_folium', 'Folium04_ContinuousWorld.ipynb']
@@ -120,11 +141,13 @@ def test_Folium04_ContinuousWorld():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_Folium05_ControlScale():
     fpath_rel = ['04_folium', 'Folium05_ControlScale.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_Folium06_FloatImage():
     fpath_rel = ['04_folium', 'Folium06_FloatImage.ipynb']
@@ -132,17 +155,20 @@ def test_Folium06_FloatImage():
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_Folium07_WMS_and_WMTS():
     fpath_rel = ['04_folium', 'Folium07_WMS_and_WMTS.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
 
+
 def test_01_cmdline():
     fpath_rel = ['09_creating_programs', '01-cmdline.ipynb']
     fname = os.path.join(nb_dir, *fpath_rel)
     tf.run_notebook(fname, clearoutput=False)
     return 0
+
 
 def test_Parallel_processing():
     fpath_rel = ['11_ParallelProcessing', 'Parallel processing.ipynb']
